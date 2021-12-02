3d4740160a747503e5dfe56a7d3efd7832058cbb
Aflah <72096386+aflah02@users.noreply.github.com>
2021-12-02 18:37:01 +0530

    Restructuring Folder

--- Assignment2/Assignment 2_1/S1.c
+++ Assignment2/Assignment 2_1/S1.c
@@ -8,7 +8,7 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include "helperS1.c"
-#include <sys/mman.h>
+
 int main(){
     key_t key = ftok("shmfile",65);
     int shmid = shmget(key,1024,0666|IPC_CREAT);
